ce737a62443c52c3e1e26b3518d759532a7d7d48
mryuvan2007-dev <mr.yuvan2007@gmail.com>
2026-06-09 20:49:51 +0530

    Time: 2 ms (91.39%), Space: 12.2 MB (96.38%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def wordBreak(self, s, wordDict):
         dp = [True] + [False] * len(s)
-       #Index: 0 1 2 3 4 5 6 7 8   
+          #Index: 0 1 2 3 4 5 6 7 8   
           #dp:    T F F F F F F F F    #by default index 0 must be true (to ensure its empty string condition)
         
 
