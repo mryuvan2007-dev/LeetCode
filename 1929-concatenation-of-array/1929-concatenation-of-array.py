c2940780c0bab8e961ee6c4c540c99b32ce91244
mryuvan2007-dev <mr.yuvan2007@gmail.com>
2026-07-31 19:16:51 +0530

    Time: 0 ms (100%), Space: 12.7 MB (34.7%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # Creates a list of zeros with a length of 2 * len(nums)
+        
+        return nums+nums
+
+
